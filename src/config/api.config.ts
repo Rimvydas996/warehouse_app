f66f7d1381569b3d54299a9f3b2f4b4ba8f85c5f
rimvydas996 <rimvydasradziukynas@gmail.com>
2025-03-06 19:31:12 +0200

    fix api.config.ts

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,4 +1,5 @@
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:5173/";
+export const API_BASE_URL =
+  import.meta.env.VITE_API_BASE_URL || "https://warehouse-liart.vercel.app";
 
 if (!API_BASE_URL) {
   throw new Error("API_BASE_URL is not set in environment variables");
